f5337a3de002d15685ba135028b489667aba4890
muellermike <mueller.doublem@gmail.com>
2022-07-18 09:34:36 +0200

    logic to load user and transcript all recordings

--- speechtotext/datalayer/recording_datalayer.py
+++ speechtotext/datalayer/recording_datalayer.py
@@ -1,12 +1,14 @@
 from datalayer.db import execute
 from models.recording import Recording
 import gc
+from base64 import b64decode
+import moviepy.editor as moviepy
 
 def load_recordings_by_user(user_id):
     """
     Load entire recording data from the database
     """
-    sql = "SELECT r.`PK`, r.`TimeToRecording`, r.`Transcript`, r.`Value`, r.`RecordingString` FROM `Recording` r JOIN ExperimentExercise ee ON ee.RecordingFK = r.PK JOIN Experiment e ON e.PK = ee.ExperimentFK JOIN User u ON u.PK = e.UserFK WHERE u.ID = %s"
+    sql = "SELECT r.`PK`, r.`TimeToRecording`, r.`Transcript`, r.`Value`, r.`RecordingString`, u.ID as UserID, ee.ExerciseFK, ee.ExperimentFK FROM `Recording` r JOIN ExperimentExercise ee ON ee.RecordingFK = r.PK JOIN Experiment e ON e.PK = ee.ExperimentFK JOIN User u ON u.PK = e.UserFK WHERE u.ID = %s"
 
     result = execute(sql, (user_id), "SELECT")
 
@@ -16,25 +18,29 @@ def load_recordings_by_user(user_id):
 
     return out_list
 
-def store_transcript():
+def store_transcript(transcript: str, recording_id):
     """
     store the transcript of one specific recording
     """
     sql = "UPDATE Recording SET Transcript = %s WHERE PK = %s"
 
-    result = execute(sql, ("test", 621), "UPDATE")
+    result = execute(sql, (transcript, recording_id), "UPDATE")
 
     return result
 
-def store_recording_to_file(filename: str):
+def store_recording_to_file(filename: str, recordingString: str):
     """
     store the recording to a file with the filename
     """
-    print(filename)
+    wav_file = open("output/wavs/temp.webm", "wb")
+    decode_string = b64decode(recordingString.split(",")[1])
+    wav_file.write(decode_string)
+
+    moviepy.ffmpeg_tools.ffmpeg_extract_audio("output/wavs/temp.webm", "output/wavs/" + filename)
     return True
 
 def make_list(recordings):
 	def apply(x):
-		return Recording(x["PK"], x["TimeToRecording"], x["Transcript"], x["Value"], x["RecordingString"])
+		return Recording(x["PK"], x["TimeToRecording"], x["Transcript"], x["Value"], x["RecordingString"], x["ExperimentFK"], x["UserID"], x["ExerciseFK"])
 
 	return list(map(apply, recordings))
